9d648dcd1f61842dbf5db5decbe4a001cc72b369
Ariadna Leon <ariadna.leon04@gmail.com>
2026-04-23 08:44:56 -0600

    feat(rive): add typing debounce and map numLook

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
+//3.1 importar timer, variables para manipular el tiempo de la animación
+import 'dart:async';
+
+
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -18,9 +23,42 @@ class _LoginScreenState extends State<LoginScreen> {
   // state machine input
   SMIBool? _isChecking;
   SMIBool? _isHandsUp;
+//2.1 variable para el recorrido de la mirada
   SMINumber? _numLook;
   SMITrigger? _trigSuccess;
   SMITrigger? _trigFail;
+
+  //1.1 para focusnode, lo roncipal es crear variables
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+
+  //3.2 variable para la mirada al dejar de escribir
+  Timer? _typingDebounce;
+
+//1.2 Agregar oyentes/chismosos 
+  @override
+  void initState() {
+    super.initState();
+    _emailFocusNode.addListener((){
+      if(_emailFocusNode.hasFocus){
+        //verificar que no se nulo
+        if(_isHandsUp != null){
+          //manos abajo en el email
+          _isHandsUp?.change(false);
+          //2.2 mirada neutral
+          _numLook?.value = 50.0;
+        }
+    }  });
+
+    _passwordFocusNode.addListener((){
+      if(_passwordFocusNode.hasFocus){
+        //manos arriba en password
+        _isHandsUp?.change(_passwordFocusNode.hasFocus);
+      }  
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -46,27 +84,52 @@ class _LoginScreenState extends State<LoginScreen> {
                       'Login Machine',
                     );
 
-                     if (_controller != null) return;
+                    if (_controller == null) return;
 
-                    //vinculando los inputs
-                    _isChecking = _controller!.findSMI('isChecking') as SMIBool;
-                    _isHandsUp = _controller!.findSMI('isHandsUp') as SMIBool;
-                    _numLook = _controller!.findSMI('numLook') as SMINumber;
+                    artboard.addController(_controller!);
+                  //vincular los inputs de la animación con las variables de flutter
+                    _isChecking =
+                        _controller!.findSMI('isChecking') as SMIBool;
+                    _isHandsUp =
+                        _controller!.findSMI('isHandsUp') as SMIBool;
+                      //2.3 vincular numlook a la animación
+                    _numLook =
+                        _controller!.findSMI('numLook') as SMINumber;
+
                     _trigSuccess =
                         _controller!.findSMI('trigSuccess') as SMITrigger;
-                    _trigFail = _controller!.findSMI('trigFail') as SMITrigger;
+                    _trigFail =
+                        _controller!.findSMI('trigFail') as SMITrigger;
                   },
                 ),
               ),
               const SizedBox(height: 10),
+
+              //1.3 Avincular focus al campo de texto
               //email
               TextField(
-                onChanged: (value) {
-                  if (_isHandsUp != null){
+                focusNode: _emailFocusNode,
+                onChanged: (value) {              
+                  if (_isHandsUp != null) {
                     _isHandsUp!.change(false);
                   }
-                  if (_isChecking == null)return;
+                  if (_isChecking == null) return;
                   _isChecking!.change(true);
+                  //2.4 implementar logica
+                  //ajuste de 0 a 100, el valor 80 es una medida de calibracion
+                  //clamp es de rango y se traduce abrsazadera
+                  final double look = (value.length / 80 * 100).clamp(0, 100);
+                  _numLook?.value = look;
+                  //3.3 reiniciar el temporizador
+                  //cancelar cualquier posible timer existente
+                  _typingDebounce?.cancel();
+                  //crear un nuevo timer
+                  _typingDebounce = Timer(const Duration(seconds: 3), () {
+                    //si se cierra la pantalla quita el contador
+                    if (!mounted) return;
+                    //mirada neutra
+                    _isChecking?.change(false);
+                  });
                 },
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
@@ -77,15 +140,17 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
+              
               //contraseña
               const SizedBox(height: 10),
               TextField(
+                focusNode: _passwordFocusNode,
                 onChanged: (value) {
-                  if (_isChecking != null){
-                    _isChecking!.change(false);
+                  if (_isChecking != null) {
+                    //_isChecking!.change(false);
                   }
-                  if (_isHandsUp == null)return;
-                  _isHandsUp!.change(true);
+                  if (_isHandsUp == null) return;
+                  //_isHandsUp!.change(true);
                 },
                 obscureText: _obscureText,
                 decoration: InputDecoration(
@@ -101,18 +166,25 @@ class _LoginScreenState extends State<LoginScreen> {
                       });
                     },
                     icon: Icon(
-                      _obscureText
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
                     ),
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 10)
             ],
           ),
         ),
       ),
     );
   }
+  //1.4 liberar recursos al salir de la pantalla
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+    //con el super dispose se asegura de que se liberen los recursos de la clase padre, evitando posibles fugas de memoria.
+  }
 }
+
